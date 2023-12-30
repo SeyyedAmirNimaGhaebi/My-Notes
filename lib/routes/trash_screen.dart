b319575159e001b13d5fe2841cd4d7af9ff9760a
Amir <ga7089036@gmail.com>
2023-12-30 21:53:30 +0330

    add appbar in trash_screen.dart

--- lib/routes/trash_screen.dart
+++ lib/routes/trash_screen.dart
@@ -1,10 +1,40 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/settings_controller.dart';
+
+final settingsController = Get.find<SettingsController>();
 
 class TrashScreen extends StatelessWidget {
   const TrashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: settingsController.backgroundColor,
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () => Get.back(),
+          child: Icon(
+            CupertinoIcons.back,
+            size: 30,
+            color: settingsController.isDark ? Colors.white : Colors.black,
+          ),
+        ),
+        foregroundColor: settingsController.isDark ? Colors.white : Colors.black,
+        title: Text(
+          'Trash',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+            color: settingsController.isDark ? Colors.white : Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: settingsController.backgroundColor,
+      ),
+    );
   }
 }
